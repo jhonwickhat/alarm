99bd6399a75e8579136948e6a7ab9dbfaa5fec40
acer <acer@DESKTOP-CEF4VBB>
2022-01-26 18:46:35 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker.Location = new System.Drawing.Point(94, 22);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(176, 22);
+            this.dateTimePicker.TabIndex = 0;
+            this.dateTimePicker.Value = new System.DateTime(2022, 1, 26, 17, 58, 21, 0);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(69, 110);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(105, 56);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(204, 110);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(93, 56);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(157, 65);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(46, 17);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 178);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dateTimePicker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Alarm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+namespace alarm
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer timer;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+
+
+        }
+
+        
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime userTime = dateTimePicker.Value;
+            if(currentTime.Hour == userTime.Hour && currentTime.Minute == userTime.Minute && currentTime.Second == userTime.Second)
+            {
+                timer.Stop();
+                try
+                {
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = @"C:\Users\acer\source\repos\alarm\a\slimshady";
+                    player.PlayLooping();
+
+
+                }
+                catch (Exception ex)
+                { MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+                
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            lblStatus.Text = "Running";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            lblStatus.Text = "Stopped";
+        }
+    }
+}
